6dc9604717a17683736d88b5adca5adda1ed0dbe
hanseltertius <hansel.tertius@gmail.com>
2017-09-08 21:37:55 +0800

    Add the class Deck, which consists of Cards in the program

--- src/Deck.java
+++ src/Deck.java
@@ -1,3 +1,24 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Deck {
-    // specify the card list (from the txt file)
+    //Create a Deck class that is based on the card lists in the .txt file
+    private ArrayList<Card> cardDeck;
+
+    Deck(ArrayList<Card> listOfcard){
+        //Constructor
+        cardDeck = listOfcard;
+    }
+
+    public Card drawnCard(){
+
+        int rc = new Random().nextInt(cardDeck.size());
+        Card getCard = cardDeck.get(rc);
+        cardDeck.remove(rc);
+        return getCard;
+    }
+
+    public ArrayList<Card> getCardDeck() {
+        return cardDeck;
+    }
 }
