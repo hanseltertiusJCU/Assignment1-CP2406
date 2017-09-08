393fc0f97c53a2c2cc9cd744418d05ef00c02a45
hanseltertius <hansel.tertius@gmail.com>
2017-09-09 04:17:04 +0800

    Add the class Player, which specify the players on the program.

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,24 @@
+import java.util.ArrayList;
+
 public class Player {
     //store player names, which depends on the number of the players
+    private ArrayList<Card> playerHand;
+    private String playerName;
+
+    Player (String name){
+        playerHand = new ArrayList<>();
+        playerName = name;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public Card getPlayerCard(int c){
+        return playerHand.get(c);
+    }
+
+    public void drawPlayerCard(Card card){
+        playerHand.add(card);
+    }
 }
